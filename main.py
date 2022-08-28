13e23f8b22a6877d6c474528aca8c52985ed87b1
Sammy Murdoch <sammy.murdoch.0@gmail.com>
2022-08-28 23:12:14 +0100

    The fibonacci mod function works

--- main.py
+++ main.py
@@ -3,33 +3,49 @@ from math import sqrt
 import numpy as np
 
 
-def matrix_power_modulus(a, n, m):
-    binary_digit_list = [int(d) for d in str(bin(n))[2:]]
-
-    current_power = a
+def generate_matrix_power_list(m, max_power, mod):
+    current_power = m
     matrix_powers = [current_power]
 
-    for i in range(len(binary_digit_list) - 1):
-        current_power = np.linalg.matrix_power(current_power, 2) % m
+    for i in range(max_power-1):
+        current_power = np.linalg.matrix_power(current_power, 2) % mod
 
         matrix_powers.append(current_power)
 
+    return matrix_powers
+
+
+def matrix_power_modulus(m, power, mod):
+    binary_digit_list = [int(d) for d in str(bin(power))[2:]]
+
+    matrix_powers = generate_matrix_power_list(m, len(binary_digit_list), mod)
+
     matrix_product = np.identity(2)
 
-    for i, matrix in matrix_powers:
+    for i, matrix in enumerate(matrix_powers[::-1]):
         if binary_digit_list[i] == 1:
-            matrix_product = np.matmul(matrix_product, matrix) % m
+            matrix_product = np.matmul(matrix_product, matrix) % mod
 
     return matrix_product
 
 
-def fibonacci_mod_m(n, m):
-    return np.matmul(matrix_power_modulus(np.array([[0, 1], [1, 1]]), n, m), np.array([[0], [1]]))[0]
+def fibonacci_mod_m(n, mod):
+    return np.matmul(matrix_power_modulus(np.array([[0, 1], [1, 1]]), n, mod), np.array([[0], [1]]))[1]
+
+#
+# for i in range(10):
+#     print(fibonacci_mod_m(i, 10))
 
 
-for i in range(10):
-    print(fibonacci_mod_m(i, 100))
+print(fibonacci_mod_m(10**14, 1234567891011))
 
 
-#print(matrix_power_modulus(np.array([[0, 1], [1, 1]]), 2, 1234567891011))
+#hi = np.array([[0, 1], [1, 1]])
+#
+#print(matrix_power_modulus(hi, 4, 2))
+#
+# print(np.matmul(hi, hi) % 2)
+
+#print(generate_matrix_power_list(hi, 2, 2)[2])
+
 
